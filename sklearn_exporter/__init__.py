19135c51e49e845aed3fb73fcc0845122dcfd2fe
Dhia Hmila <dhiahmila@gmail.com>
2021-01-08 20:38:23 +0100

    added DecisionTreeClassifier

--- sklearn_exporter/__init__.py
+++ sklearn_exporter/__init__.py
@@ -15,9 +15,12 @@ def load(file):
 
 from sklearn.linear_model import LinearRegression
 from sklearn.neural_network import MLPClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 
 sklearn_piskle_partializer.register_class_attributes(LinearRegression, ['coef_', 'intercept_'])
+sklearn_piskle_partializer.register_class_attributes(DecisionTreeClassifier, ['n_features_', 'n_outputs_', 'classes_',
+                                                                              'tree_'])
 sklearn_piskle_partializer.register_class_attributes(MLPClassifier, ['n_features_in_', '_label_binarizer', 'classes_',
                                                                      'n_outputs_', 'n_layers_', 'out_activation_',
                                                                      'coefs_', 'intercepts_'])
