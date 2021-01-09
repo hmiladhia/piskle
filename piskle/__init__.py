044ac6da368b3e0e2f24f7b33d00cd9c523a696f
amal-hasni <amal.hasni@hotmail.fr>
2021-01-09 22:50:59 +0100

    added __version__ variable

--- piskle/__init__.py
+++ piskle/__init__.py
@@ -5,6 +5,8 @@ from piskle._piskle_initializer import init_partializer as _init_partializer
 sklearn_piskle_partializer = _init_partializer(SklearnPisklePartializer())
 sklearn_exporter = Pisklizer(sklearn_piskle_partializer)
 
+__version__ = '0.0.1'
+
 
 def dump(model, file, *args, **kwargs):
     return sklearn_exporter.dump(model, file, *args, **kwargs)
