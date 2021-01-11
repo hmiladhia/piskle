d315514fbc4ae216c151e26cbb7def4053216835
Dhia Hmila <dhiahmila@gmail.com>
2021-01-11 19:36:17 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -5,17 +5,19 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="piskle",
-    version="0.0.1",
+    version="0.0.2.dev1",
     author="Amal HASNI, Dhia HMILA",
     author_email="emerald.snippets@gmail.com",
     description="Piskle allows you to selectively and efficiently serialize scikit-learn models "
                 "to save on memory and load times.",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    license='MIT',
     url="https://github.com/hmiladhia/piskle",
     packages=['piskle'],
     install_requires=['scikit-learn==0.24.0'],
     classifiers=[
+        "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
