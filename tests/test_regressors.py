f3f82455940146b23e9c9156b4ec4f6f7f4fbe44
Dhia Hmila <dhiahmila@gmail.com>
2021-10-10 17:13:18 +0200

    added regression tests

---
+++ tests/test_regressors.py
@@ -0,0 +1,40 @@
+import warnings
+
+import pytest
+from sklearn import datasets
+from sklearn.linear_model import LinearRegression, Lasso, Ridge
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.neural_network import MLPRegressor
+from sklearn.tree import DecisionTreeRegressor
+
+import piskle
+
+
+def information_loss(uploaded_model, model, X_test, y_test):
+    score1 = uploaded_model.score(X_test, y_test)
+    score2 = model.score(X_test, y_test)
+    return score1 == score2
+
+
+@pytest.mark.parametrize('model_class', [
+    LinearRegression,
+    DecisionTreeRegressor,
+    MLPRegressor,
+    KNeighborsRegressor,
+    Lasso,
+    Ridge,
+])
+def test_regression_models(model_class):
+    X, y = datasets.load_diabetes(return_X_y=True)
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        model = model_class().fit(X, y)
+
+    model_bytes = piskle.dumps(model)
+    piskle_model = piskle.loads(model_bytes)
+
+    assert information_loss(piskle_model, model, X, y)
+
+
+
