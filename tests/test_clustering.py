b76776c8800df2e19316ba3b0dc14131f4e4e37b
Dhia Hmila <dhiahmila@gmail.com>
2021-10-10 17:50:54 +0200

    added clustering tests

---
+++ tests/test_clustering.py
@@ -0,0 +1,31 @@
+import warnings
+
+import pytest
+from sklearn import datasets
+from sklearn.cluster import KMeans
+from sklearn.mixture import GaussianMixture
+
+import piskle
+
+
+def information_loss(model1, model2, X_test):
+    y_pred1 = model1.predict(X_test)
+    y_pred2 = model2.predict(X_test)
+    return (y_pred1 == y_pred2).all()
+
+
+@pytest.mark.parametrize('model_class', [
+    KMeans,
+    GaussianMixture,
+])
+def test_unsupervised_models(model_class):
+    X, _ = datasets.load_iris(return_X_y=True)
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        model = model_class().fit(X)
+
+    model_bytes = piskle.dumps(model)
+    piskle_model = piskle.loads(model_bytes)
+
+    assert information_loss(piskle_model, model, X)
